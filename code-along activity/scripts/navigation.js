28d03c34f0a01dd92b2916ea17db80e980ba31d7
Happiness-Isaac <mmabahappiness@gmail.com>
2026-03-10 19:00:09 +0100

    Code-Along

---
+++ code-along activity/scripts/navigation.js
@@ -0,0 +1,8 @@
+const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+// Toggle the show class off and on
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
